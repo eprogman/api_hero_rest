c5fe3f6e8dd289ad11682dd800e522d206606a60
luism <luism@py.com>
2024-12-05 13:21:18 -0500

    quinto commit

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/superhero', (req, res) => {
 
     //const origin = req.header('origin')
     //res.header('Access-Control-Allow-Origin', origin)
-    //res.header('Access-Control-Allow-Origin', 'https://leohero.netlify')
+
+    res.header('Access-Control-Allow-Origin', 'https://leohero.netlify')
 
     const { page } = req.query
     const { limit } = req.query
@@ -37,7 +38,7 @@ app.get('/superhero/:name', (req, res) => {
     //const origin = req.header('origin')
     //res.header('Access-Control-Allow-Origin', origin)
 
-    //res.header('Access-Control-Allow-Origin', 'https://leohero.netlify')
+    res.header('Access-Control-Allow-Origin', 'https://leohero.netlify')
 
     const { name } = req.params
     const superh = superhero.filter(hero => hero.name.toLowerCase().match(name.toLowerCase()))
