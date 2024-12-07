87776d01937e2ad4aaeee359f267e83447971349
luism <luism@py.com>
2024-12-07 15:57:11 -0500

    commit dieciseisavo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const superhero = require('./superhero.json')
+const superhero = require('./JSON/superhero.json')
 
 const app = express();
 app.use(express.json())
